fdf5b1844bc1c4ead3cdd4d583bb8112d1dab89f
Murzayev Chin <Murzayev@gmail.com>
2014-12-15 09:51:00 +0000

    finish project

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
   
   def create
@@ -46,8 +47,21 @@ class UsersController < ApplicationController
     flash[:success] = "User deleted."
     redirect_to users_url
   end
-
   
+def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.followed_users.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
   private
   
   def user_params
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,18 +6,44 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: {minimum: 6}
   
   has_secure_password
+  has_many :microposts, dependent: :destroy
+  has_many :relationships, foreign_key: :follower_id,dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+  has_many :reverse_relationships, foreign_key: :followed_id, dependent: :destroy, class_name: "Relationship"
+  has_many :followers, through: :reverse_relationships, source: :follower
   
   before_save { self.email = self.email.downcase }
   before_create :create_remember_token
   
+  
   def User.new_remember_token
     SecureRandom.urlsafe_base64
   end
-
+  
+ def feed
+    # Это предварительное решение. См. полную реализацию в "Following users".
+    Micropost.where("user_id = ?", id)
+  end
+  
   def User.encrypt(token)
     Digest::SHA1.hexdigest(token.to_s)
   end
-
+    
+  def feed
+    Micropost.from_users_followed_by(self)
+  end
+  
+  def follow!(other_user)
+    relationships.create(follower_id: id, followed_id: other_user.id)
+  end
+  
+  def unfollow!(other_user)
+    relationships.find_by(follower_id: id, followed_id: other_user.id).destroy
+  end
+  
+  def following?(other_user)
+    relationships.find_by(follower_id: id, followed_id: other_user.id)
+  end
   private
 
     def create_remember_token
